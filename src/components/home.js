22f0784dade9d9fa2553bf0fe5fe2a40a313487b
Dillan Simmons <dillans@yesler.com>
2018-01-20 09:53:34 -0800

    adjusted nav and fonts

--- src/components/home.js
+++ src/components/home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     return (
       <div className="holder">
         <div className="home">
-          <h1 className="title">Wine <br />Calc</h1>
+          <h1 className="title">Wine Calc</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
